dce1ad56ad14c97b92696f6b0bd36c4f78b286a9
Jorge Montesinos <osmarmontes50@gmail.com>
2023-05-31 21:31:32 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+export class ProductManager {
+  products;
+  constructor() {
+    this.products = [];
+  }
+
+  #findBiggerId() {
+    let biggerId = 0;
+    this.products.forEach((product) => {
+      if (product.id > biggerId) {
+        biggerId = product.id;
+      }
+    });
+    return biggerId;
+  }
+
+  #productExists(code) {
+    return this.products.find((product) => product.code === code);
+  }
+
+  addProduct({ title, description, price, thumbnail, code, stock }) {
+    if (this.#productExists(code)) {
+      console.log("El producto ya existe")
+      return;
+    }
+    const id = this.#findBiggerId() + 1;
+    this.products.push({
+      id: id,
+      title: title,
+      description: description,
+      price: price,
+      thumbnail: thumbnail,
+      code: code,
+      stock: stock,
+    });
+  }
+
+  getProducts() {
+    if (this.products.length === 0) {
+        console.log("No hay productos cargados");
+      return;
+    }
+    return this.products;
+  }
+
+  getProductById(id) {
+    const product = this.products.find((product) => product.id === parseInt(id));
+    if (!product) {
+      return { error: "Not found" };
+    }
+    return product;
+  }
+}
---
+++ test.js
@@ -0,0 +1,47 @@
+import { ProductManager } from "./index.js";
+
+const productManager = new ProductManager();
+
+console.log({ productManager });
+
+const product1 = {
+    title: "Producto prueba",
+    description: "Este es un producto de prueba",
+    price: 200,
+    thumbnail: "Sin imagen",
+    code: "abc1234",
+    stock: 25,
+}
+
+const product2 = {
+    title: "Producto prueba 2",
+    description: "Este es un producto de prueba 2",
+    price: 400,
+    thumbnail: "Sin imagen",
+    code: "abc1235",
+    stock: 5,
+}
+
+productManager.addProduct(product1);
+productManager.addProduct(product2);
+
+const products = productManager.getProducts();
+
+const product3 = {
+    title: "Producto prueba",
+    description: "Este es un producto de prueba",
+    price: 200,
+    thumbnail: "Sin imagen",
+    code: "abc1234",
+    stock: 25,
+}
+
+productManager.addProduct(product3);
+
+console.log({ products });
+
+const productSearched = productManager.getProductById(1);
+console.log({ productSearched });
+
+const productNotFounded = productManager.getProductById(5);
+console.log({ productNotFounded });
